b81c15a04f28f40569e9b5c218fb2014d876673d
michael-conway <michael_conway@unc.edu>
2016-12-09 12:19:59 -0500

    #1 add script to add user when the uid and password already set

---
+++ utils/irods-provisioning/add_to_existing_irods_user.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USER="$1"
+ZONE="$2"
+DE_RODSADMIN="$3"
+
+
+
+if [ "$#" -ne 3 ]; then
+   echo "usage: $0: userName zoneName iRodsAdministratorUser"
+   echo "ex: $0 testde8 dsai_renci rods"
+   exit 1;
+fi
+
+echo "allow irods ownership of $USER for now"
+ichmod -M own rods /$ZONE/home/$USER
+
+echo "create $USER trash folder"
+imkdir /$ZONE/trash/home/$DE_RODSADMIN/$USER
+
+echo "set $USER ownership of trash folder"
+ichmod -M own $USER /$ZONE/trash/home/$DE_RODSADMIN/$USER
+
+echo "set inheritance on trash folder"
+ichmod -M inherit /$ZONE/trash/home/$DE_RODSADMIN/$USER
+
+echo "make $USER/analyses so DE doesn't throw error on login"
+imkdir /$ZONE/home/$USER/analyses
+
+echo "set $USER ownership of analyses"
+ichmod -M own $USER /$ZONE/home/$USER/analyses
+
+echo "set inheritance on analyses"
+ichmod inherit /$ZONE/home/$USER/analyses
+
